4eb50955b7f79b7126908fff9374e40605d8fc12
Rodrigo Angelo Valentini <rodrigoangelovalentini@hotmail.com>
2016-08-02 11:50:43 -0300

    fim basico

--- doctrine-basico/public/index.php
+++ doctrine-basico/public/index.php
@@ -1,4 +1,5 @@
 <?php
 require_once '../vendor/autoload.php';
 
+require_once '../src/doctrine.php';
 require_once '../src/routes.php';
--- doctrine-basico/src/routes.php
+++ doctrine-basico/src/routes.php
@@ -4,6 +4,9 @@ use Zend\Diactoros\ServerRequestFactory;
 use Aura\Router\RouterContainer;
 use Zend\Diactoros\Response;
 use Slim\Views\PhpRenderer;
+use App\Entity\Category;
+use App\Entity\Post;
+use Psr\Http\Message\ServerRequestInterface;
 
 $request = ServerRequestFactory::fromGlobals(
   $_SERVER,$_GET,$_POST,$_COOKIE,$_FILES
@@ -11,17 +14,35 @@ $request = ServerRequestFactory::fromGlobals(
 
 $routerContainer = new RouterContainer();
 
+$generator = $routerContainer->getGenerator();
 $map = $routerContainer->getMap();
 
 $view = new PhpRenderer('../templates/');
 
-$map->get('home','/',function($request,$response) use($view){
-    return $view->render($response,'home.phtml');
+$entityManager = getEntityManager();
+
+$map->get('home','/',function(ServerRequestInterface $request,$response) use($view, $entityManager){
+    $postRepository = $entityManager->getRepository(Post::class);
+    $categoryRepository = $entityManager->getRepository(Category::class);
+    $categories = $categoryRepository->findAll();
+
+    $data = $request->getQueryParams();
+    if(isset($data['search']) and $data['search']!=''){
+        $queryBuilder = $postRepository->createQueryBuilder('p');
+        $queryBuilder->join('p.categories','c')->where($queryBuilder->expr()->eq('c.id',$data['search']));
+        $posts = $queryBuilder->getQuery()->getResult();
+    }else{
+        $posts = $postRepository->findAll();
+    }
+
+    return $view->render($response,'home.phtml',[
+        'posts' => $posts,
+        'categories' => $categories
+    ]);
 });
 
-$map->get('categories','/categories',function($request,$response) use($view){
-    return $view->render($response,'categories/list.phtml');
-});
+include_once 'categories.php';
+include_once 'posts.php';
 
 $matcher = $routerContainer->getMatcher();
 
@@ -35,5 +56,10 @@ $callable = $route->handler;
 
 $response = $callable($request, new Response());
 
-echo $response->getBody();
+if($response instanceof Response\RedirectResponse){
+    header("location:{$response->getHeader("location")[0]}");
+}else if($response instanceof Response){
+    echo $response->getBody();
+}
+
 
